bfc2b4186ad45b3b35496f241c729e464f27f38b
benjamin <benjakre@live.com>
2022-04-13 20:41:25 -0300

    Añadido pie de página

--- pdf_reports.py
+++ pdf_reports.py
@@ -1,38 +1,96 @@
+import datetime
 from table_class import PDF
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import pandas
 
 
 class CrearReportePDF:
     image_path = 'images/logo.png'
+    encabezados = ['AUTOR:','PACIENTE:','FEC.NAC.:','TIPO DOCUMENTO:','NRO. DOCUMENTO:','SEXO:']
 
-    def crear_reporte(self, tabla_del_paciente: pandas.DataFrame, filepath, *args, **kwargs):
-        all_data = self.formatear(tabla_del_paciente.to_dict())
+    def crear_reporte(self, datos_de_mediciones: pandas.DataFrame, filepath, datos_encabezado, *args, **kwargs):
+        nombre_del_servicio = None
+        all_data = self.formatear(datos_de_mediciones.to_dict())
 
         selected_data = all_data
 
         # selected_data = {}
         # for arg in args:
         #     selected_data[arg] = all_data[arg]
+        # nombre_completo = kwargs.get("nombre_completo")
 
         pdf = PDF()
+        pdf.alias_nb_pages()
         pdf.add_page()
 
-        pdf.set_font("Times", size=10)
-
+        if "datos_del_instituto" in kwargs:
+            datos_del_instituto = kwargs.get("datos_del_instituto")
+            nombre_del_servicio = datos_del_instituto[0]
         if "image" in kwargs:
             self.image_path = kwargs.get("image")
-        w, h = Image.open(self.image_path).size
-        factor = w // 75
+        img = Image.open(self.image_path)
+
+        w, h = img.size
+        factor = h // 25
         w //= factor
         h //= factor
 
-        pdf.set_font(family="Times", size=24, style="B")
-        pdf.image(self.image_path, h=h, w=w)
-        pdf.cell(w=0, h=0, txt="Pacientes", border=0, align="C", ln=1)
+        pdf.image(self.image_path, w=w, h=h)
 
-        pdf.set_font("Times", size=10)
-        pdf.create_table(table_data=selected_data, title='I\'m the first title', cell_width='even')
+        pdf.set_xy(200, pdf.t_margin + 5)
+        pdf.set_font(family="Times", size=9)
+        index = 0
+        while pdf.y < pdf.t_margin + h + 3:
+            try:
+                dato = datos_del_instituto[index]
+            except IndexError:
+                break
+            except UnboundLocalError:
+                break
+            else:
+                if dato is not None:
+                    pdf.cell(w=0, h=0, txt=f"{dato}", border=0, align="R")
+                    pdf.set_y(pdf.y + 5)
+                index += 1
+
+        pdf.set_x(pdf.l_margin)
+
+        if pdf.y < pdf.t_margin + h:
+            pdf.set_y(pdf.t_margin + h + 3)
+
+        if nombre_del_servicio is not None:
+            pdf.set_font("Helvetica", size=8, style="B")
+            pdf.cell(w=46, h=5, txt="SERVICIO:", border=0, align="L")
+            pdf.set_font("Helvetica", size=8)
+            if datos_encabezado[0] is None:
+                pdf.cell(w=46, h=5, txt=f"{nombre_del_servicio}", border=0, align="L")
+            else:
+                pdf.cell(w=46, h=5, txt=f"{nombre_del_servicio}", border=0, align="L",ln=1)
+                pdf.set_font("Helvetica", size=8, style="B")
+                pdf.cell(w=46, h=5, txt=f"{self.encabezados[0]}", border=0, align="L")
+                pdf.set_font("Helvetica", size=8)
+                pdf.cell(w=46, h=5, txt=f"{datos_encabezado[0]}", border=0, align="L")
+
+        pdf.set_font("Helvetica", size=8, style="B")
+        pdf.cell(w=46, h=5, txt=f"FECHA: ", border=0, align="L")
+        pdf.set_font("Helvetica", size=8)
+        pdf.cell(w=46, h=5, txt=datetime.datetime.now().strftime("%d/%m/%Y"), border=0, align="L", ln=1)
+
+        data_counter = 0
+        for dato,encabezado in zip(datos_encabezado[1:],self.encabezados[1:]):
+            data_counter += 1
+            if dato is not None:
+                pdf.set_font("Helvetica", size=8, style="B")
+                pdf.cell(w=46, h=5, txt=encabezado, border=0, align="L")
+                pdf.set_font("Helvetica", size=8)
+                if data_counter % 2 == 0:
+                    pdf.cell(w=46, h=5, txt=dato, border=0, align="L", ln=1)
+                else:
+                    pdf.cell(w=46, h=5, txt=dato, border=0, align="L")
+
+        pdf.cell(w=46, h=5, txt="", border=0, align="L", ln=1)
+
+        pdf.create_table(table_data=selected_data, title='Resultados', cell_width='even')
         pdf.ln()
 
         pdf.output(f"{filepath}")
